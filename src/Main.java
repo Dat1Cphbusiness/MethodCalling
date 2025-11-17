63442062d199bdb8b6a7c76f766fa2c94ee08b82
tessG <tess@lovegreen.life>
2025-11-17 10:13:49 +0100

    instructions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
 public class Main {
     // Main method to start the program
     public static void main(String[] args) {
+
+        // INSTRUKTIONER TIL STUDERENDE:
+        // -------------------------------
+        // Når du starter et nyt level, skal du:
+        // 1. Fjern "//" foran de to linjer for det level du arbejder på
+        // 2. Tilføj "//" foran de to linjer for det forrige level
+        //
+        // Eksempel: Når du går fra level 1 til level 2:
+        //   - Tilføj "//" foran "Robot myRobot = new Robot();" og "myRobot.run();"
+        //   - Fjern "//" foran "MessageMaker maker = new MessageMaker();" og "maker.run();"
+        //
+        // Kun ét level skal være aktivt ad gangen!
+
+
+        // Level 1
         Robot myRobot = new Robot();
         myRobot.run();
+
+        // Level 2
+        //MessageMaker maker = new MessageMaker();
+        //maker.run();
+
+        // Level 3
+        //InfoDisplay display = new InfoDisplay();
+        //display.run();
+
+        // Level 4
+        //DataProcessor processor = new DataProcessor();
+        //processor.run();
+
+        // Level 5
+        //Calculator calculator = new Calculator();
+        //calculator.run();
+
     }
 }
